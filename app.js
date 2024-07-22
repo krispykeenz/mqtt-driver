1dccdfc3cf34ae87342c26bd171de61e474d7b6e
krispykeenz <37831909@student.g.nwu.ac.za>
2024-07-22 09:17:58 +0200

    Simple app connecting to broker

--- app.js
+++ app.js
@@ -1,23 +1,18 @@
 const mqtt = require('mqtt');
 
-// MQTT broker URL
-const brokerUrl = 'mqtt://155.93.192.206:18083';
+const brokerUrl = 'mqtt://155.93.192.206:1883';
 
-// Create an MQTT client
 const client = mqtt.connect(brokerUrl);
 
-// Handle connection event
 client.on('connect', () => {
   console.log('Connected to MQTT broker');
 
-  // Subscribe to a topic
   const topic = 'test/topic';
   client.subscribe(topic, (err) => {
     if (!err) {
       console.log(`Subscribed to ${topic}`);
 
-      // Publish a message to the topic
-      const message = 'Hello, MQTT!';
+      const message = 'Hi my name is Keenan';
       client.publish(topic, message, (err) => {
         if (!err) {
           console.log(`Published message: ${message}`);
@@ -27,12 +22,10 @@ client.on('connect', () => {
   });
 });
 
-// Handle incoming messages
 client.on('message', (topic, message) => {
   console.log(`Received message on ${topic}: ${message.toString()}`);
 });
 
-// Handle errors
 client.on('error', (err) => {
   console.error('MQTT error:', err);
 });
